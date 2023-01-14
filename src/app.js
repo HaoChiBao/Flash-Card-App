839989482f49492c53e1cbac569f4d16354ea2b9
James Yang <jamesyang663@gmail.com>
2023-01-14 15:31:47 -0500

    frenzy game skeleton complete

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import Register from './app/authentication/register';
 import Login from './app/authentication/login';
 import CreateFC from './app/dashboard/flashcard/createFC';
 import GroupFC from './app/dashboard/flashcard/groupFC';
+import Frenzy from './app/games/frenzy/frenzy';
 import { Route, Routes } from 'react-router-dom';
 import Landing from './app/landing/landing';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/create" element={<CreateFC />} />
         <Route path="/groups" element={<GroupFC />} />
+        <Route path="/frenzy" element={<Frenzy />} />
     </Routes>);
 }
 
--- src/app/games/frenzy/frenzy.jsx
+++ src/app/games/frenzy/frenzy.jsx
@@ -2,6 +2,8 @@ import './frenzy.css';
 import {doc, getDoc, updateDoc} from 'firebase/firestore/lite';
 import {System} from '../../../firebase/config';
 
+import { onLoad, addTimer } from './logic';
+
 const system = new System();
 
 let state = true 
@@ -9,53 +11,31 @@ let state = true
 let uid = localStorage.getItem('flash-card-uid')
 function Frenzy(){
   window.onload = function() {
-  if (state) {
+    if (state) {
 
-    // this will be dynamically set so the user can choose which group to study
-    const selectedGroup = 'CS'
-    const frenzy = document.getElementById('frenzy')
-    const frenzyTimer = frenzy.querySelector('.timer')
+      // this will be dynamically set so the user can choose which group to study
+      const selectedGroup = 'CS'
 
-    const ref = doc(system.db, 'users', uid)
-    const field = getDoc(ref).then((doc) => {
-    const data = doc.data()
-    const flashcards = data.flashcards
-    
-    console.log(flashcards[selectedGroup])
+      const frenzy = document.getElementById('frenzy')
+      const frenzyTimer = frenzy.querySelector('.timer')
+      const game = document.getElementById('cards')
 
-    // let timeout = setTimeout(() => {
-    //   console.log('hello')
-    // }, 1000)
-    
-      const timer = {
-        prev: Date.now(),
-        min: 0,
-        sec: 0,
-        ms: 0,
-        total: 0,
-      }
+      const ref = doc(system.db, 'users', uid)
+      const field = getDoc(ref).then((doc) => {
+      const data = doc.data()
+      const flashcards = data.flashcards
       
-      let setLoop = setInterval(() => {
-        const now = Date.now() - timer.prev
-        timer.prev = Date.now()
+      console.log(flashcards[selectedGroup])
 
-        timer.total += now
-
-        timer.ms = Math.round((timer.total%1000)/10)
-        timer.sec = Math.floor(timer.total/1000)
-        if(timer.total/1000 >= 60) {
-          timer.min += 1;
-          timer.total = 0;
-        }
-
-        frenzyTimer.innerText = `${timer.min}:${timer.sec}:${timer.ms}`
-        
-      }, 150)
+      onLoad(game, flashcards[selectedGroup])
+    
+      addTimer(frenzyTimer)
           
     })
   }
 
-  }
+
+}
   return (
     <div id="frenzy">
         <div className="timer"></div>
