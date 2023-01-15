b1a075caf317d39ff5464efd876bd62cd219d023
Judy Zhou <dzhou224@uwo.ca>
2023-01-15 08:29:28 -0500

    redid home page

--- src/app/landing/landing.jsx
+++ src/app/landing/landing.jsx
@@ -5,8 +5,8 @@ import { Link } from "react-router-dom";
 
 function Landing() {
     window.onload = function () {
-        const signUp = document.querySelector('.sign-up');
-        const login = document.querySelector('.login');
+        const signUp = document.querySelector('.homesign-up');
+        const login = document.querySelector('.login1');
 
         signUp.addEventListener('click', () => {
             window.location.assign('/register');
@@ -19,25 +19,36 @@ function Landing() {
 
     return (
         <div className="bodyFr">
-            <div className="container">
+            <div>
+                <div className="login-box">
+                    <a className="login1">Login</a>
+                </div>
+                <div className="container">
+                    <div className="texty">
+                        <div className="title1">Elevate your learning with <span className="special1">active recall</span> and <span className="special1">mini-games</span></div>
+                        <div className="doomed">Because falling asleep on your desk is so last semester.</div>
+                        <div>
+                            <button className="homesign-up" >Get Started</button>
+                        </div>
+
+                    </div>
+
+                    <img src="./sign-up/cc.png"></img>
+
+                    {/* <div></div>
+                <div>
+                    <button className="login" >Login</button>
+                </div>
                 <div className="text-section">
                     <div className="title1">Study Sprint</div>
                     <div className="description"> Elevate your learning with active recall and mini-games</div>
-                    {/* <Link to="/register"> */}
-                    <div>
-                        <button className="sign-up" >Sign Up</button>
-                    </div>
-                    {/* </Link> */}
 
-                    {/* <Link to="login"> */}
+                    
                     <div>
-                        <button className="login" >Login</button>
+                        <img src="mainslayer.png" />
                     </div>
-                    {/* </Link> */}
-                </div>
-                <div>
-                    <img src="mainslayer.png" />
-                </div>
+                </div> */}
+                </div >
             </div>
         </div>
     );
